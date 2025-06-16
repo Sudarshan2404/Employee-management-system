28708d932b983c4ac90a22ec16a24ab250a683f2
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-06-16 17:32:45 +0530

    Employee Dashboard complete

--- src/components/Tasklist/TaskList.jsx
+++ src/components/Tasklist/TaskList.jsx
@@ -3,12 +3,71 @@ import React from "react";
 const TaskList = () => {
   return (
     <div className="tasklist h-[55%] flex flex-nowrap justify-start overflow-x-auto items-center gap-5 w-full py-5 mt-10">
-      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl"></div>
-      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl"></div>
-      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl"></div>
-      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl"></div>
-      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl"></div>
-      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl"></div>
+      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl">
+        <div className="flex justify-between py-2 px-3">
+          <h2 className="bg-red-600 py-1 px-2 rounded-sm text-sm">High</h2>
+          <h2 className="text-sm">26-06-2025</h2>
+        </div>
+        <h2 className="text-xl font-semibold p-4">Make a youtube Video</h2>
+        <p className="text-sm px-2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          voluptatibus magnam maxime voluptatem, aspernatur hic aliquam minus
+          blanditiis dignissimos assumenda asperiores adipisci illo ducimus
+          similique minima quo cupiditate dolorum necessitatibus.
+        </p>
+      </div>
+      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl">
+        <div className="flex justify-between py-2 px-3">
+          <h2 className="bg-red-600 py-1 px-2 rounded-sm text-sm">High</h2>
+          <h2 className="text-sm">26-06-2025</h2>
+        </div>
+        <h2 className="text-xl font-semibold p-4">Make a youtube Video</h2>
+        <p className="text-sm px-2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          voluptatibus magnam maxime voluptatem, aspernatur hic aliquam minus
+          blanditiis dignissimos assumenda asperiores adipisci illo ducimus
+          similique minima quo cupiditate dolorum necessitatibus.
+        </p>
+      </div>
+      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl">
+        <div className="flex justify-between py-2 px-3">
+          <h2 className="bg-red-600 py-1 px-2 rounded-sm text-sm">High</h2>
+          <h2 className="text-sm">26-06-2025</h2>
+        </div>
+        <h2 className="text-xl font-semibold p-4">Make a youtube Video</h2>
+        <p className="text-sm px-2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          voluptatibus magnam maxime voluptatem, aspernatur hic aliquam minus
+          blanditiis dignissimos assumenda asperiores adipisci illo ducimus
+          similique minima quo cupiditate dolorum necessitatibus.
+        </p>
+      </div>
+      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl">
+        <div className="flex justify-between py-2 px-3">
+          <h2 className="bg-red-600 py-1 px-2 rounded-sm text-sm">High</h2>
+          <h2 className="text-sm">26-06-2025</h2>
+        </div>
+        <h2 className="text-xl font-semibold p-4">Make a youtube Video</h2>
+        <p className="text-sm px-2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          voluptatibus magnam maxime voluptatem, aspernatur hic aliquam minus
+          blanditiis dignissimos assumenda asperiores adipisci illo ducimus
+          similique minima quo cupiditate dolorum necessitatibus.
+        </p>
+      </div>
+      <div className="flex-shrink-0 w-[300px] bg-yellow-400 h-full rounded-xl">
+        <div className="flex justify-between py-2 px-3">
+          <h2 className="bg-red-600 py-1 px-2 rounded-sm text-sm">High</h2>
+          <h2 className="text-sm">26-06-2025</h2>
+        </div>
+        <h2 className="text-xl font-semibold p-4">Make a youtube Video</h2>
+        <p className="text-sm px-2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          voluptatibus magnam maxime voluptatem, aspernatur hic aliquam minus
+          blanditiis dignissimos assumenda asperiores adipisci illo ducimus
+          similique minima quo cupiditate dolorum necessitatibus.
+        </p>
+      </div>
     </div>
   );
 };
